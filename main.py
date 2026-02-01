50ea5507fa10d901bdb84eece8d0956c3b0be5c6
prabhsuratsingh <prabhsurat17@gmail.com>
2026-02-01 19:34:03 +0530

    Create Transformer block

--- main.py
+++ main.py
@@ -38,3 +38,28 @@ class SelfAttention(nn.Module):
         out = self.fc_out(out)
 
         return out
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion*embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion*embed_size, embed_size)
+        )
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, value, key, query, mask):
+        attention = self.attention(value, key, query, mask)
+
+        Z = self.dropout(self.norm1(attention + query))
+        forward = self.feed_forward(Z)
+        X = self.dropout(self.norm2(forward + Z))
+
+        return X
