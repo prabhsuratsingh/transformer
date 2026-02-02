39e7fd1be2458c6d6219425772c7fcded28f227f
prabhsuratsingh <prabhsurat17@gmail.com>
2026-02-02 16:39:37 +0530

    Create Transformer

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from networkx import from_prufer_sequence, tadpole_graph
 import torch
 import torch.nn as nn
 
@@ -168,3 +167,68 @@ class Decoder(nn.Module):
             x = layer(x, encoder_out, encoder_out, src_mask, target_mask)
 
         out = self.fc_out(x)
+
+class Transformer(nn.Module):
+    def __init__(
+            self,
+            src_vocab_size,
+            target_vocab_size,
+            src_pad_index,
+            target_pad_index,
+            embed_size=256,
+            num_layers=6,
+            forward_expansion=4,
+            heads=8,
+            dropout=0,
+            device="cuda",
+            max_length=100
+    ):
+        super(Transformer, self).__init__()
+
+        self.encoder = Encoder(
+            src_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            device,
+            forward_expansion,
+            dropout,
+            max_length
+        )
+
+        self.decoder = Decoder(
+            target_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            forward_expansion,
+            dropout,
+            device,
+            max_length
+        )
+
+        self.src_pad_index = src_pad_index
+        self.target_pad_index = target_pad_index
+        self.device = device
+
+    def make_src_mask(self, src):
+        src_mask = (src != self.src_pad_index).unsqueeze(1).unsqueeze(2)
+        return src_mask.to(self.device)
+
+    def make_target_mask(self, target):
+        N, target_len = target.shape
+        target_mask = torch.tril(torch.ones((target_len, target_len))).expand(
+            N, 1, target_len, target_len
+        )
+
+        return target_mask.to(self.device)
+    
+    def forward(self, src, target):
+        src_mask = self.make_src_mask(src)
+        target_mask = self.make_target_mask(src)
+
+        encoder_src = self.encoder(src, src_mask)
+
+        out = self.decoder(target, encoder_src, src_mask, target_mask)
+
+        return out
